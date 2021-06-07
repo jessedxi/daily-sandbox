e22a7251a39788d61d85dd8e55b41e296fe5f9af
Joseph Blais <josephpierreblais@gmail.com>
2021-06-07 14:59:23 -0400

    create handleNewMeetingState function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,9 +12,43 @@ const STATE_ERROR = 'STATE_ERROR';
 
 
 function App() {
+  const [appState, setAppState] = useState(STATE_IDLE);
+  const [roomUrl, setRoomUrl] = useState(null)
+  const [callObject, setCallObject] = useState(null)
 
   // Meeting State represents where the user is in the lifecycle of their participation in a call.
-  // This bit of code updates the meeting state 
+  // This useEffect function updates the meeting state:
+
+  useEffect(() => {
+    if (!callObject) return;
+
+    const events = ['joined-meeting', 'left-meeting', 'error'];
+
+    // In the demo he puts a parameter called event in this function, but it seems like
+    // he's only using to console.log something so I didn't put it.
+    // If everything's broken put it back in.
+    function handleNewMeetingState() {
+      switch (callObject.meetingState()) {
+        case 'joined-meeting':
+          setAppState(STATE_JOINED);
+         break;
+        case 'left-meeting':
+          callObject.destroy().then(() => {
+            setRoomUrl(null);
+            setCallObject(null);
+            setAppState(STATE_IDLE)
+          })
+          break;
+        case 'error':
+          setAppState(STATE_ERROR);
+          break;
+        default:
+         break;
+      }
+    }
+
+    
+  })
 
   return (
     <div className="App">
