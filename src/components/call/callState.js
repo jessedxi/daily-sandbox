f99fb89c21c146339447f4fc3ce9c12a275064c7
Jesse <jessedaniel@live.ca>
2021-06-08 12:22:04 -0600

    implement getCallItems function

--- src/components/call/callState.js
+++ src/components/call/callState.js
@@ -68,3 +68,24 @@ function callReducer(callState, action) {
       throw new Error();
   }
 }
+
+function getLocalCallItem(callItems) {
+  return callItems["local"];
+}
+
+function getCallItems(participants) {
+  let callItems = { ...initialCallState.callItems }; // Ensure we always have a local participant
+  for (const [id, participant] of Object.entries(participants)) {
+    callItems[id] = {
+      videoTrackState: participant.tracks.video,
+      audioTrackState: participant.tracks.audio,
+    };
+    if (shouldIncludeScreenCallItem(participant)) {
+      callItems[id + "-screen"] = {
+        videoTrackState: participant.tracks.screenVideo,
+        audioTrackState: participant.tracks.screenAudio,
+      };
+    }
+  }
+  return callItems;
+}
