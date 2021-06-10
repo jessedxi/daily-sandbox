732df98ed6e2767786f8ae1cd609e075cf67aeb0
Jesse <jessedaniel@live.ca>
2021-06-10 14:05:29 -0600

    add helpers functions/export

--- src/components/call/callState.js
+++ src/components/call/callState.js
@@ -103,5 +103,53 @@ function shouldIncludeScreenCallItem(participant) {
 // True if id corresponds to local participant (*not* their screen to share)
 
 function isLcoal(id) {
+  return id === "local";
+}
+
+function isScreenShare(id) {
   return id.endsWith("-screen");
 }
+
+function containsScreenShare(callItems) {
+  return Object.keys(callItems).some((id) => isScreenShare(id));
+}
+
+function getMessage(callState) {
+  function shouldShowClickAllow() {
+    const localCallItem = getLocalCallItem(callState.callItems);
+    const hasLoaded = localCallItem && !localCallItem.isLoading;
+    return !hasLoaded && callState.clickAllowTimeoutFired;
+  }
+
+  let header = null;
+  let detail = null;
+  let isError = false;
+  if (callState.fatalError) {
+    header = `Fatal error: ${callState.fatalError};`;
+    isError = true;
+  } else if (callState.camOrMicError) {
+    header = `Camera or mic access error: ${callState.camOrMicError}`;
+    detail =
+      "See https://help.daily.co/en/articles/2528184-unblock-camera-mic-access-on-a-computer to troubleshoot.";
+    isError = true;
+  } else if (shouldShowClickAllow()) {
+    header = 'Click "Allow" to enable camera and mic access.';
+  } else if (Object.kets(callState.callItems).length === 1) {
+    header = "Copy and share this page's URL to invite others";
+    detail = window.location.href;
+  }
+  return header || detail ? { header, detail, isError } : null;
+}
+
+export {
+  initialCallState,
+  CLICK_ALLOW_TIMEOUT,
+  PARTICIPANTS_CHANGE,
+  CAM_OR_MIC_ERROR,
+  FATAL_ERROR,
+  callReducer,
+  isLcoal,
+  isScreenShare,
+  containsScreenShare,
+  getMessage,
+};
