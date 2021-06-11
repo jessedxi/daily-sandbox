13412d158df062907f2a2a1caefdb5a338a118d1
Joseph Blais <josephpierreblais@gmail.com>
2021-06-11 15:34:39 -0400

    Add Tile component

---
+++ src/components/Tile/Tile.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useMemo, useRef } from 'react';
+
+/**
+ * Props
+ * - videoTrackState: DailyTrackState?
+ * - audioTrackState: DailyTrackState?
+ * - isLocalPerson: boolean
+ * - isLarge: boolean
+ * - disableCornerMessage: boolean
+ * - onClick: Function
+ */
