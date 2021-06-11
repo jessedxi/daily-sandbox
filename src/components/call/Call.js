38727833d35a7262809d6213611140db5143dfe5
Joseph Blais <josephpierreblais@gmail.com>
2021-06-11 15:27:15 -0400

    Add preliminary call functionality

--- src/components/call/Call.js
+++ src/components/call/Call.js
@@ -1,5 +1,17 @@
 import React, { useEffect, useContext, useReducer, useCallback } from "react";
 import CallObjectContext from "../../CallObjectContext";
+import {
+  initialCallState,
+  CLICK_ALLOW_TIMEOUT,
+  PARTICIPANTS_CHANGE,
+  CAM_OR_MIC_ERROR,
+  FATAL_ERROR,
+  callReducer,
+  isLocal,
+  isScreenShare,
+  containsScreenShare,
+  getMessage,
+} from './callState';
 
 export default function Call() {
   const callObject = useContext(CallObjectContext);
@@ -15,10 +27,10 @@ export default function Call() {
     ];
 
     function handleNewParticipantsState(event) {
-      //event && logDailyEent(event);
+      //event && logDailyEvent(event);
       dispatch({
         type: PARTICIPANTS_CHANGE,
-        participants: callOBject.participants(),
+        participants: callObject.participants(),
       });
     }
 
@@ -33,8 +45,43 @@ export default function Call() {
     //Stop Listening for changes in state
     return function cleanup() {
       for (const event of events) {
-        callObject.off(event, handleNewParticipantsState);
+        callObject && callObject.off(event, handleNewParticipantsState);
       }
     };
   }, [callObject]);
+  
+  // SEND an app message to the remote participant whose tile was clicked on
+  const sendHello = useCallback(
+    (participantId) => {
+      callObject && callObject.sendAppMessage({hello: 'world'}, participantId)
+    }
+  )
+
+
+  function getTiles() {
+    let largeTiles = [];
+    let smallTiles = [];
+    Object.entries(callState.callItems).forEach(([id, callItem]) => {
+      const isLarge =
+      isScreenShare(id) ||
+      (!isLocal(id) && !containsScreenShare(callState.callItems));
+      const title = (
+        <Tile
+          key={id}
+          videoTrackState={callItem.videoTrackState}
+          audioTrackState={callItem.audioTrackState}
+          isLocalPerson={isLocal(id)}
+          isLarge={isLarge}
+          disableCornerMessage={isScreenShare(id)}
+          onClick={
+            isLocal(id)
+            ? null 
+            : () => {
+              sendHello(id)
+            }
+          }
+        ></Tile>
+      )
+    })
+  }
 }
