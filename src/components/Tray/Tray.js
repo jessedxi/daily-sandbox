184c3c85a2dad14597d23eca92c4ded25b1db560
Jesse <jessedaniel@live.ca>
2021-06-13 11:51:40 -0600

    add getStremStates helper

--- src/components/Tray/Tray.js
+++ src/components/Tray/Tray.js
@@ -11,3 +11,31 @@ import Chat from "../Chat/Chat";
 import CallObjectContext from "../../CallObjectContext";
 import { logDailyEvent } from "../../logUtils";
 import DailyIframe from "@daily-co/daily-js";
+
+/**
+ * Gets [isCameraMuted, isMicMuted, isSharingScreen].
+ * This function is declared outside Tray() so it's not recreated every render
+ * (which would require us to declare it as a useEffect dependency).
+ */
+function getStreamStates(callObject) {
+  let isCameraMuted,
+    isMicMuted,
+    isSharingScreen = false;
+  if (
+    callObject &&
+    callObject.participants() &&
+    callObject.participants().local
+  ) {
+    const localParticipant = callObject.participants().local;
+    isCameraMuted = !localParticipant.video;
+    isMicMuted = !localParticipant.audio;
+    isSharingScreen = !localParticipant.screen;
+  }
+  return [isCameraMuted, isMicMuted, isSharingScreen];
+}
+
+/**
+ * Props:
+ * - onClickLeaveCall: () => ()
+ * - disabled: boolean
+ */
